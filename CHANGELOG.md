33dcf9730d2a47ca0990a88330ec01c018bd2b78
Rusty <rusty@quantumweb.co>
2026-04-10 08:46:42 +0100

    fix: icon

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@
 
 ## [Unreleased]
 
+## [0.0.2] - 2026-04-10
+### Added
+- Added a dedicated file icon for `.edge` templates.
+
 ## [0.0.1] - 2026-04-08
 ### Added
 - Initial public release of `edgejs-jetbrains` for JetBrains IDEs.
--- src/main/kotlin/com/github/quantumweb/edgejsjetbrains/edgejsintellij/edge/EdgeFileType.kt
+++ src/main/kotlin/com/github/quantumweb/edgejsjetbrains/edgejsintellij/edge/EdgeFileType.kt
@@ -2,7 +2,6 @@ package com.github.quantumweb.edgejsjetbrains.edge
 
 import com.intellij.ide.highlighter.XmlLikeFileType
 import com.intellij.openapi.fileTypes.TemplateLanguageFileType
-import javax.swing.Icon
 
 object EdgeFileType : XmlLikeFileType(EdgeLanguage), TemplateLanguageFileType {
     override fun getName(): String = "Edge"
@@ -11,5 +10,5 @@ object EdgeFileType : XmlLikeFileType(EdgeLanguage), TemplateLanguageFileType {
 
     override fun getDefaultExtension(): String = "edge"
 
-    override fun getIcon(): Icon? = null
+    override fun getIcon() = EdgeIcons.FILE
 }
